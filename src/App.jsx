fb1c2f3b9f2020031fa3199175b5096091f8f453
allthatjava <allthatjava@yahoo.ca>
2026-03-14 21:28:16 -0400

    Sync Navbar look with main site

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { usePdfCompressor } from './hooks/usePdfCompressor'
 import { PdfCompressorView } from './components/PdfCompressorView'
+import NavBar from './components/NavBar'
 import './App.css'
 
 function App() {
   const props = usePdfCompressor()
-  return <PdfCompressorView {...props} />
+  return (
+    <div className="page">
+      <NavBar />
+      <main className="main">
+        <div className="container">
+          <div className="card">
+            <PdfCompressorView {...props} />
+          </div>
+        </div>
+      </main>
+    </div>
+  )
 }
 
 export default App
